a39ef3cf812143666dac389e5b8039e34090de84
Jonathan Tran <jpt017@ucsd.edu>
2019-02-28 12:15:23 -0800

    finished entire lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = '6c2k8cP7oQhFA8pVP7HaTcCI3CRYQscn'; 
+	
+	var map = L.mapquest.map('map',{
+		center: [32.8792208, -117.2392868],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 14,
+		zoomControl: false
+	});
+	
+	L.marker([32.88, -117.236]).addTo(map);
 }
